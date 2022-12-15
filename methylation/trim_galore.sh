a959665e79834dd01efc986b2822c4ec012c7d72
Chandler Sutherland <chandlersutherland@berkeley.edu>
2022-12-14 21:41:34 -0800

    fixing slurm status

--- methylation/trim_galore.sh
+++ methylation/trim_galore.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+#SBATCH --job-name=methylation_processing
+#SBATCH --partition=savio4_htc
+#SBATCH --qos=savio_normal
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=01:00:00
+#SBATCH --mail-user=chandlersutherland@berkeley.edu
+#SBATCH --mail-type=ALL
+#SBATCH --error=/global/home/users/chandlersutherland/slurm_stderr/slurm-%j.out
+#SBATCH --output=/global/home/users/chandlersutherland/slurm_stdout/slurm-%j.out
 
 module load cutadapt
 module load fastqc 
