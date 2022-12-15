7f60e4b84d499ca6b51db3eb72f644393c83a2f5
Chandler Sutherland <chandlersutherland@berkeley.edu>
2022-12-14 21:26:21 -0800

    starting compilation file, updating trim_galore

--- methylation/trim_galore.sh
+++ methylation/trim_galore.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
-#SBATCH --job-name=trim_galore
-#SBATCH --partition=savio2
-#SBATCH --qos=savio_normal
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=24
-#SBATCH --time=02:00:00
-#SBATCH --mail-user=chandlersutherland@berkeley.edu
-#SBATCH --mail-type=ALL
-#SBATCH --error=/global/home/users/chandlersutherland/slurm_stderr/slurm-%j.out
-#SBATCH --output=/global/home/users/chandlersutherland/slurm_stdout/slurm-%j.out
 
 module load cutadapt
 module load fastqc 
 
+#set the trim directory, output directory, and input directory, containing the untrimmed fastq files
 TRIM_DIR=/global/home/users/chandlersutherland/programs/TrimGalore-0.6.6
-OUT_DIR=/global/scratch/users/chandlersutherland/e14/trim_williams/rna/
-rna='SRR17281236 SRR17281235 SRR17281234 SRR17281233'
+trim_output=/global/scratch/users/chandlersutherland/e14/trim_williams/pooled
+trim_input=/global/scratch/users/chandlersutherland/e14/bs_fastq_files/williams_pooled
 
-cd /global/scratch/users/chandlersutherland/e14/rna_fastq_files/williams/
-
-for f in $rna
+#run trim galore in default mode 
+for f in $trim_input/*
 do 
-   $TRIM_DIR/trim_galore -o $OUT_DIR --fastqc --illumina "${f}".fastq 
+   BASENAME=$(basename $f .fastq)
+   $TRIM_DIR/trim_galore -o $trim_output --fastqc --illumina $f
+   echo "finished trimming ${BASENAME}"
 done 
