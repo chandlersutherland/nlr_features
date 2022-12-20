78359d519eb0f5ef3696ed2470c4a0b1824108f8
Chandler Sutherland <chandlersutherland@berkeley.edu>
2022-12-20 14:59:20 -0800

    over my head

--- methylation/trim_galore.sh
+++ methylation/trim_galore.sh
@@ -5,8 +5,8 @@ module load fastqc
 
 #set the trim directory, output directory, and input directory, containing the untrimmed fastq files
 TRIM_DIR=/global/home/users/chandlersutherland/programs/TrimGalore-0.6.6
-trim_output=/global/scratch/users/chandlersutherland/e14/trim_williams/pooled
-trim_input=/global/scratch/users/chandlersutherland/e14/bs_fastq_files/williams_pooled
+#trim_output=/global/scratch/users/chandlersutherland/e14/trim_williams/pooled
+#trim_input=/global/scratch/users/chandlersutherland/e14/bs_fastq_files/williams_pooled
 
 #run trim galore in default mode 
 for f in $trim_input/*
