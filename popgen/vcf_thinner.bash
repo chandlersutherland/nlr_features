272c70cb38209e6a47fc017c803b12d692ae30d8
Chandler Sutherland <chandlersutherland@berkeley.edu>
2023-09-28 09:39:25 -0700

    updated comment goal

--- popgen/vcf_thinner.bash
+++ popgen/vcf_thinner.bash
@@ -13,19 +13,14 @@
 module load vcftools
 cd /global/scratch/users/chandlersutherland/e14/popgen/vcf_1001_full
 
-#vcftools --vcf 1001genomes_snp-short-indel_only_ACGTN.vcf --keep nlrome_IDs.txt --recode --recode-INFO-all --out nlrome_partial 
+#goal: subset the giant all sites VCF by the NLRome accessions and by the CDS sequences
+#create one file per chromosome to run in parallel 
+
 bed='/global/scratch/users/chandlersutherland/Athaliana/Atha_cds.bed'
 gvcf_input='1001genomes_snp-short-indel_with_tair10_only_ACGTN.vcf.gz'
-#vcftools --gzvcf $gvcf_input --chr 1 --keep nlrome_IDs.txt --recode --bed $bed --out nlrome_invar_1_cds 
-#vcftools --gzvcf $gvcf_input --chr 2 --keep nlrome_IDs.txt --recode --bed $bed --out nlrome_invar_2_cds
+vcftools --gzvcf $gvcf_input --chr 1 --keep nlrome_IDs.txt --recode --bed $bed --out nlrome_invar_1_cds 
+vcftools --gzvcf $gvcf_input --chr 2 --keep nlrome_IDs.txt --recode --bed $bed --out nlrome_invar_2_cds
 vcftools --gzvcf $gvcf_input --chr 3 --keep nlrome_IDs.txt --recode --bed $bed --out nlrome_invar_3_cds
 vcftools --gzvcf $gvcf_input --chr 4 --keep nlrome_IDs.txt --recode --bed $bed --out nlrome_invar_4_cds
 vcftools --gzvcf $gvcf_input --chr 5 --keep nlrome_IDs.txt --recode --bed $bed --out nlrome_invar_5_cds
 
-#repeat with gene bed file
-#bed_gene='/global/scratch/users/chandlersutherland/Athaliana/Atha_genes.recode.bed'
-#vcftools --vcf nlrome_partial.recode.vcf --chr 1 --recode --bed $bed_gene --out nlrome_partial_1_gene
-#vcftools --vcf nlrome_partial.recode.vcf --chr 2 --recode --bed $bed_gene --out nlrome_partial_2_gene
-#vcftools --vcf nlrome_partial.recode.vcf --chr 3 --recode --bed $bed_gene --out nlrome_partial_3_gene
-#vcftools --vcf nlrome_partial.recode.vcf --chr 4 --recode --bed $bed_gene --out nlrome_partial_4_gene
-#vcftools --vcf nlrome_partial.recode.vcf --chr 5 --recode --bed $bed_gene --out nlrome_partial_5_gene
